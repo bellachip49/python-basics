f189afb0f61067cc62c512042515a3227f00a23f
Bella Chip <krystal.m.kwan@gmail.com>
2016-05-14 15:52:01 -0700

    make file

---
+++ calculator/__main__.py
@@ -0,0 +1,33 @@
+# Krystal Kwan
+# May 14, 2016
+# A calculator that adds and subtracts two numbers.
+
+def adding(num1, num2):
+    add = num1 + num2
+    print("%i + %i = %i" % (num1, num2, add))
+
+def subtract(num1, num2):
+    sub = num1 - num2
+    print("%i - %i = %i" % (num1, num2, sub))
+
+def decide(ans, num1, num2):
+    
+
+def menu():
+    print("Add or subtract?")
+    print("\t1) Addition")
+    print("\t2) Subtract")
+    ans = input(">>> ")
+    print("First Number: ")
+    num1 = int(input(">>> "))
+    print("First Number: ")
+    num1 = int(input(">>> "))
+    except ValueError:
+        print ("Input Invalid.")
+    decide(ans, num1, num2)
+
+def main():
+    print("hello world")
+
+if __name__ == '__main__':
+    main()
